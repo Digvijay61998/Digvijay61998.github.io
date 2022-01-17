370d8aeeb37cb5e7358f3d13911f6e7c6f140a8e
Jay <digvijaykadam61998@gmail.com>
2022-01-17 12:50:36 +0530

    set the struture

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,39 +5,39 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>OTHER CITIES WE SERVE</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-9.jpg'
-              text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
+              src='images/bangalore.jpg'
+              text='Services in Bangalore'
+              label='truck in bangalore'
               path='/services'
             />
             <CardItem
-              src='images/img-2.jpg'
-              text='Travel through the Islands of Bali in a Private Cruise'
+              src='images/channai.jpg'
+              text='Services in Chennai'
               label='Luxury'
               path='/services'
             />
           </ul>
           <ul className='cards__items'>
             <CardItem
-              src='images/img-3.jpg'
-              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              src='images/mumbai.jpg'
+              text='Services in Mumbai'
               label='Mystery'
               path='/services'
             />
             <CardItem
-              src='images/img-4.jpg'
-              text='Experience Football on Top of the Himilayan Mountains'
+              src='images/pune.jpeg'
+              text='Services in Pune'
               label='Adventure'
               path='/products'
             />
             <CardItem
-              src='images/img-8.jpg'
-              text='Ride through the Sahara Desert on a guided camel tour'
+              src='images/Rajasthan.jpg'
+              text='Services in Rajasthan'
               label='Adrenaline'
               path='/sign-up'
             />
